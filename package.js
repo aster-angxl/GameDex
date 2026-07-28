1a728d61037f43f0cbf565359b31661619f59258
aster-angxl <system.cloudy123@gmail.com>
2026-07-28 21:00:55 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,12 @@
-{"name":"gamedex-bot", "version":"1.0.0", "description":"Bot Discord GameDex", "main":"index.js", "scripts":{"start":"node index.js"}}
+{
+  "name": "gamedex-bot",
+  "version": "1.0.0",
+  "description": "Bot Discord GameDex",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.21.0"
+  }
+      }
