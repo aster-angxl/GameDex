ad05ddd87f7dcaf6f09ecb6c8b9e143b842409a4
aster-angxl <system.cloudy123@gmail.com>
2026-07-28 20:59:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("GameDex démarre!")
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Connecté en tant que ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
