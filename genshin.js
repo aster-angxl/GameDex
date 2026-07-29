0f02f6aa2d0dbee476893c346d2edf1cbece7c27
aster-angxl <system.cloudy123@gmail.com>
2026-07-29 10:54:57 +0200

    Create genshin.js

---
+++ genshin.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("build")
+    .setDescription("Affiche un build Genshin")
+    .addStringOption(option =>
+      option
+        .setName("personnage")
+        .setDescription("Nom du personnage")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const personnage = interaction.options.getString("personnage");
+
+    await interaction.reply(
+      `La fiche de build de **${personnage}** est en préparation !`
+    );
+  }
+};
