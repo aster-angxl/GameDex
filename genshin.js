aa3be9e7619b0997ea1518a4bcef42f5b88c2431
aster-angxl <system.cloudy123@gmail.com>
2026-07-29 20:33:23 +0200

    Update genshin.js

--- genshin.js
+++ genshin.js
@@ -12,8 +12,61 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    const personnage = interaction.options.getString("personnage");
+    const personnage = interaction.options.getString("personnage").toLowerCase();
 
+    const builds = {
+      venti: {
+        nom: "Venti",
+        texte :
+          **Venti**
+
+        Global Rating : S
+        ------------------------
+        Main DPS : S
+        Sub-DPS : S
+        Support : -
+        Exploration : SS
+       ------------------------
+         **Hexrei Main DPS**
+
+         **Best Weapon**
+         The Daybreak Chronicles
+         
+         **Replacement Weapon**
+         1.Astral Vulture's Crimson
+         2.Aqua Simulacra
+         3.Compound Bow
+
+         **Best Artifacts**
+         A Day Carved from Rising Winds (4pcs)
+           
+         **Artifact Main Stats**
+           -Sands : ATK%
+           -Goblet : Anemo DMG Bonus
+           -Circlet : CRIT Rate or CRIT DMG
+
+         **Artifact Sub Stats**
+           -Elemental Mastery
+           -Energy Recharge
+           -ATK%
+           -CRIT Rate
+           -CRIT DMG
+           
+         **Sample Team**
+            Venti-Durin-Faruzan-Bennett
+  }
+};
+
+if (builds[personnage])
+{
+  await
+    interaction.reply({
+      content :
+        builds[personnage]. texte,
+      ephemeral:true,
+    });
+}else{
+    
     await interaction.reply({
       content: `La fiche de build de **${personnage}** est en préparation !`,
       ephemeral: true,
