68dce8d89c88037ca1c589e84e537c950f0ebb2a
aster-angxl <system.cloudy123@gmail.com>
2026-08-01 06:45:57 +0200

    Update zibai.js

--- data/Genshin/characters/Liyue/zibai.js
+++ data/Genshin/characters/Liyue/zibai.js
@@ -1,1 +1,71 @@
+module.exports = {
 
+    nom: "Zibai",
+
+    source: "Game8",
+
+    update: "01/08/2026",
+
+    embeds: [
+
+        {
+            title: "🐉 Zibai - Informations",
+
+            description: `
+⭐ **Global Rating:** SS
+
+⚔️ **Main DPS:** SS
+🔹 **Sub-DPS:** -
+🛡️ **Support:** -
+🗺️ **Exploration:** B
+`
+        },
+
+
+        {
+            title: "⚔️ Zibai - Builds & Équipements",
+
+            description: `
+## 🌙 Lunar-Crystallize Main DPS
+
+⚔️ **Best Weapon**
+Lightbearing Moonshard
+
+
+🔄 **Replacement Weapons**
+1. Uraku Misugiri
+2. Light of Foliar Incision
+3. Peak Patrol Song
+4. Harbinger of Dawn (F2P)
+5. Flute of Ezpitzal (Craftable)
+6. Absolution
+7. Cinnabar Spindle
+
+
+📖 **Best Artifacts**
+Night of the Sky's Unveiling (4pcs)
+
+
+📊 **Main Stats**
+• Sands: DEF %
+• Goblet: DEF %
+• Circlet: CRIT Rate / CRIT DMG
+
+
+📈 **Sub Stats**
+• CRIT Rate
+• CRIT DMG
+• DEF %
+• Elemental Mastery
+• Energy Recharge
+
+
+🎯 **Goal Stats**
+
+• DEF: 2,500+
+• CRIT Rate: 40~50% (Before Night of the Sky's Unveiling bonus)
+• CRIT DMG: 175%+
+• Elemental Mastery: 100-200
+• Energy Recharge: 110-130%
+`
+        },
